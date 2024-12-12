835a5e6422095ff80fc42429baa90d7e10119613
LucasCABJ <lucascaraballo15@gmail.com>
2024-12-12 14:19:52 -0300

    FEAT: Added PostRepository

---
+++ src/main/java/com/bootcamp/social_meli/repository/IPostRepository.java
@@ -0,0 +1,6 @@
+package com.bootcamp.social_meli.repository;
+
+import com.bootcamp.social_meli.model.Post;
+
+public interface IPostRepository extends ICrudRepository<Post, Long> {
+}
---
+++ src/main/java/com/bootcamp/social_meli/repository/impl/PostRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.bootcamp.social_meli.repository.impl;
+
+import com.bootcamp.social_meli.model.Post;
+import com.bootcamp.social_meli.repository.IPostRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PostRepositoryImpl implements IPostRepository {
+
+    private final List<Post> postsList = new ArrayList<>();
+
+    @Override
+    public Post create(Post obj) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return postsList;
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Post delete(Post obj) {
+        return null;
+    }
+}
