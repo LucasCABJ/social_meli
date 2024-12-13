ad4cceff153924adab52098dd4e54b773c0ad8ab
Franmuslera <francisco.muslera@gmail.com>
2024-12-13 17:42:27 -0300

    Agrego creacion de promoPost

--- src/main/java/com/bootcamp/social_meli/service/IPostService.java
+++ src/main/java/com/bootcamp/social_meli/service/IPostService.java
@@ -1,8 +1,10 @@
 package com.bootcamp.social_meli.service;
 
 import com.bootcamp.social_meli.dto.PostDTO;
+import com.bootcamp.social_meli.dto.PromoPostDTO;
 import com.bootcamp.social_meli.dto.response.UserPostResponse;
 
 public interface IPostService {
     UserPostResponse createPost(PostDTO postDTO);
+    UserPostResponse createPromoPost(PromoPostDTO promoPostDTO);
 }
