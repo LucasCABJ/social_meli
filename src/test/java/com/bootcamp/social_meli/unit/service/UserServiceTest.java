0d68288f4f9269bf346806c16ba32208d2fb95b7
“rivejjj” <rivero.joaquinn@gmail.com>
2024-12-30 17:29:02 -0300

    test with zero followers returns correct amount

--- src/test/java/com/bootcamp/social_meli/unit/service/UserServiceTest.java
+++ src/test/java/com/bootcamp/social_meli/unit/service/UserServiceTest.java
@@ -1,10 +1,43 @@
 package com.bootcamp.social_meli.unit.service;
 
+import com.bootcamp.social_meli.model.User;
+import com.bootcamp.social_meli.repository.IPostRepository;
+import com.bootcamp.social_meli.repository.IUserRepository;
+import com.bootcamp.social_meli.repository.impl.PostRepositoryImpl;
+import com.bootcamp.social_meli.repository.impl.UserRepositoryImpl;
+import com.bootcamp.social_meli.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    @Mock
+    IUserRepository userRepository;
+    @Mock
+    IPostRepository postRepository;
+    @Mock
+    ObjectMapper objectMapper;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
 
     @Test
     void findAll() {
@@ -35,7 +68,15 @@ class UserServiceTest {
     }
 
     @Test
-    void getFollowerCount() {
+    void getFollowerCountOfUserWithoutFollowersReturnsZero() {
+        Long expectedAmount = 0L;
+        User user = new User(1L, "Juan", "Juan", "juancito", List.of(), List.of());
+        when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
+        UserServiceImpl userService = new UserServiceImpl(postRepository, userRepository, objectMapper);
+
+        Long amount = userService.getFollowerCount(1L).getFollowers_count();
+
+        Assertions.assertEquals(expectedAmount, amount);
     }
 
     @Test
