4e8a66eb963fda308ea921dcd5d5fe27992d15e3
nicoezdias <nicolasezdias@gmail.com>
2024-12-30 12:39:23 -0300

    feat: Refactor package test

---
+++ src/test/java/com/bootcamp/social_meli/unit/service/PostServiceTest.java
@@ -0,0 +1,46 @@
+package com.bootcamp.social_meli.unit.service;
+
+import org.junit.jupiter.api.Test;
+
+class PostServiceTest {
+
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void createPromoPost() {
+    }
+
+    @Test
+    void updatePromoPost() {
+    }
+
+    @Test
+    void getPostsWithProduct() {
+    }
+
+    @Test
+    void createUserResponse() {
+    }
+
+    @Test
+    void mostPostsUsers() {
+    }
+
+    @Test
+    void testMostPostsUsers() {
+    }
+
+    @Test
+    void getPostsByPriceRange() {
+    }
+
+    @Test
+    void parseStringToDouble() {
+    }
+
+    @Test
+    void convertToPostDTO() {
+    }
+}
---
+++ src/test/java/com/bootcamp/social_meli/unit/service/ProductServiceTest.java
@@ -0,0 +1,28 @@
+package com.bootcamp.social_meli.unit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getAllPostFollowsLastTwoWeeksUnordered() {
+    }
+
+    @Test
+    void getAllPostsFollowsLastTwoWeeks() {
+    }
+
+    @Test
+    void getMostProducts() {
+    }
+
+    @Test
+    void testGetMostProducts() {
+    }
+
+    @Test
+    void getAmountOfPromosByUser() {
+    }
+}
