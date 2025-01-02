f55d2c509c5b6331c0cc849c6322c61777b2eeda
“rivejjj” <rivero.joaquinn@gmail.com>
2025-01-02 09:51:47 -0300

    fixed tests configuration error

--- src/test/java/com/bootcamp/social_meli/unit/service/UserServiceTest.java
+++ src/test/java/com/bootcamp/social_meli/unit/service/UserServiceTest.java
@@ -1,12 +1,8 @@
 package com.bootcamp.social_meli.unit.service;
 
 import com.bootcamp.social_meli.model.User;
-import com.bootcamp.social_meli.repository.IPostRepository;
 import com.bootcamp.social_meli.repository.IUserRepository;
-import com.bootcamp.social_meli.repository.impl.PostRepositoryImpl;
-import com.bootcamp.social_meli.repository.impl.UserRepositoryImpl;
 import com.bootcamp.social_meli.service.impl.UserServiceImpl;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,30 +10,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     @Mock
     IUserRepository userRepository;
-    @Mock
-    IPostRepository postRepository;
-    @Mock
-    ObjectMapper objectMapper;
-
     @InjectMocks
     UserServiceImpl userService;
 
-    @Autowired
-    private UserServiceImpl userServiceImpl;
 
     @Test
     void findAll() {
@@ -72,7 +58,19 @@ class UserServiceTest {
         Long expectedAmount = 0L;
         User user = new User(1L, "Juan", "Juan", "juancito", List.of(), List.of());
         when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
-        UserServiceImpl userService = new UserServiceImpl(postRepository, userRepository, objectMapper);
+
+        Long amount = userService.getFollowerCount(1L).getFollowers_count();
+
+        Assertions.assertEquals(expectedAmount, amount);
+    }
+
+    @Test
+    void getFollowerCountOfUserWithTwoFollowersReturnsTwo() {
+        Long expectedAmount = 2L;
+        User follower1 = new User(2L, "Tom", "Tom", "tomito", List.of(), List.of());
+        User follower2 = new User(2L, "Mati", "Mati", "mati123", List.of(), List.of());
+        User user = new User(1L, "Juan", "Juan", "juancito", List.of(), List.of(follower1,follower2));
+        when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
 
         Long amount = userService.getFollowerCount(1L).getFollowers_count();
 
