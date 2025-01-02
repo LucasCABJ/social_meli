49de84fd85d33a37b99e66530a0c35fc52902ce1
“rivejjj” <rivero.joaquinn@gmail.com>
2025-01-02 12:31:33 -0300

    add tests for postService getPostsWithProduct

--- src/test/java/com/bootcamp/social_meli/unit/service/PostServiceTest.java
+++ src/test/java/com/bootcamp/social_meli/unit/service/PostServiceTest.java
@@ -1,8 +1,29 @@
 package com.bootcamp.social_meli.unit.service;
 
+import com.bootcamp.social_meli.dto.response.PostsWithProductResponseDTO;
+import com.bootcamp.social_meli.model.Post;
+import com.bootcamp.social_meli.repository.impl.PostRepositoryImpl;
+import com.bootcamp.social_meli.service.impl.PostServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class PostServiceTest {
+
+    @Mock
+    PostRepositoryImpl postRepository;
+    @InjectMocks
+    PostServiceImpl postService;
 
     @Test
     void createPost() {
@@ -17,7 +38,29 @@ class PostServiceTest {
     }
 
     @Test
+    @DisplayName("La cantidad de posts de un producto deberia ser cero si no hay posts con ese producto")
+    void getPostsWithProductReturnsZero() {
+        Integer expectedAmount = 0;
+        String productName = "Taza";
+        when(postRepository.getPostsWithProduct(Mockito.anyString())).thenReturn(List.of());
+
+        PostsWithProductResponseDTO response = postService.getPostsWithProduct(productName);
+
+        Assertions.assertEquals(expectedAmount, response.getPosts().size());
+    }
+
+    @Test
+    @DisplayName("La cantidad de posts de un producto deberia ser dos si hay dos posts con ese producto")
     void getPostsWithProduct() {
+        Integer expectedAmount = 2;
+        String productName = "Taza";
+        Post post1 = new Post();
+        Post post2 = new Post();
+        when(postRepository.getPostsWithProduct(Mockito.anyString())).thenReturn(List.of(post1, post2));
+
+        PostsWithProductResponseDTO response = postService.getPostsWithProduct(productName);
+
+        Assertions.assertEquals(expectedAmount, response.getPosts().size());
     }
 
     @Test
