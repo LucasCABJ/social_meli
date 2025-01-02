caf59bc64cf360c336b7ca744cf1a5c415588e55
“rivejjj” <rivero.joaquinn@gmail.com>
2025-01-02 10:07:33 -0300

    add test for service exception handling

--- src/test/java/com/bootcamp/social_meli/unit/service/UserServiceTest.java
+++ src/test/java/com/bootcamp/social_meli/unit/service/UserServiceTest.java
@@ -1,9 +1,11 @@
 package com.bootcamp.social_meli.unit.service;
 
+import com.bootcamp.social_meli.exception.NotFoundException;
 import com.bootcamp.social_meli.model.User;
 import com.bootcamp.social_meli.repository.IUserRepository;
 import com.bootcamp.social_meli.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -54,6 +56,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("La cantidad de seguidores de un usuario sin seguidores es 0")
     void getFollowerCountOfUserWithoutFollowersReturnsZero() {
         Long expectedAmount = 0L;
         User user = new User(1L, "Juan", "Juan", "juancito", List.of(), List.of());
@@ -65,9 +68,10 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("La cantidad de seguidores de un usuario con dos seguidores es 2")
     void getFollowerCountOfUserWithTwoFollowersReturnsTwo() {
         Long expectedAmount = 2L;
-        User follower1 = new User(2L, "Tom", "Tom", "tomito", List.of(), List.of());
+        User follower1 = new User(3L, "Tom", "Tom", "tomito", List.of(), List.of());
         User follower2 = new User(2L, "Mati", "Mati", "mati123", List.of(), List.of());
         User user = new User(1L, "Juan", "Juan", "juancito", List.of(), List.of(follower1,follower2));
         when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
@@ -75,6 +79,14 @@ class UserServiceTest {
         Long amount = userService.getFollowerCount(1L).getFollowers_count();
 
         Assertions.assertEquals(expectedAmount, amount);
+    }
+
+    @Test
+    void getFollowerCountOfInexistentUserThrowsException(){
+        Long userId = 0L;
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            userService.getFollowerCount(userId);
+        });
     }
 
     @Test
